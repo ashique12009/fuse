01854382a666cc11c127d0ae20fdb8aa3642ec96
ashique-ubuntu <ashique12009@gmail.com>
2018-05-28 00:00:02 +0600

    register page done

--- js/custom.js
+++ js/custom.js
@@ -1,8 +1,16 @@
 // Home page js
 (function ($) {
   // Jquery code goes here
+  // Login code
   $('.user-login-form input[type="text"]').attr('placeholder', 'Username');
   $('.user-login-form input[type="password"]').attr('placeholder', 'Password');
+  
+  // Register code
+  $('.user-register-form input[type="email"]').attr('placeholder', 'Email');
+  $('.user-register-form input[name="name"]').attr('placeholder', 'Username');
+  $('.user-register-form input[name="pass[pass1]"]').attr('placeholder', 'Password');
+  $('.user-register-form input[name="pass[pass2]"]').attr('placeholder', 'Confirm Password');
+  
   // Sidebar
   $('.close-me').on('click', function(){
     $('.page-sidebar').hide();
